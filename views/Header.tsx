35268c2d0639854371cf6cdf6acc6d5515efa80c
prosperomage <omage.prosper@gmail.com>
2025-09-20 05:16:05 +0100

    setup for cloudadore website

---
+++ views/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="max-w-7xl outline">
+      <section>top-most</section>
+      <section>
+        <div>ClOUDADORE</div>
+        <div>nav-links</div>
+        <div>buttons</div>
+      </section>
+    </div>
+  );
+};
+
+export default Header;
